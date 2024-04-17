c3091ecec8f8746b8612593bf370b06c7d9052c6
Maxim93mk <kinjashov.m@rambler.ru>
2024-04-17 17:09:42 +0300

    Добавлены кнопки для задач

--- src/components/tasks/tasks.jsx
+++ src/components/tasks/tasks.jsx
@@ -3,7 +3,7 @@ import TaskItem from "./task-item";
 import '../tasks/css/tasks.css'
 
 
-function TasksMain({data, mainColor, borderColor, cardsTitle, cardsIcons}) {
+function TasksMain({ data, mainColor, borderColor, cardsTitle, cardsIcons }) {
 
     function randomNumber(min, max) {
         let rand = min + Math.random() * (max + 1 - min);
@@ -19,17 +19,28 @@ function TasksMain({data, mainColor, borderColor, cardsTitle, cardsIcons}) {
             return <TaskItem taskName={elem} mainColor={mainColor} status={data[0].status[statusIndex]} user={data[0].user[userIndex]} />
         }
     });
-
+    function test(event, x) {
+        console.log(event.target)
+      return document.querySelectorAll('.tasks-block').item(2).append( <TaskItem taskName={"elem"} mainColor={mainColor} status={"data[0].status[statusIndex]"} user={"data[0].user[userIndex]"} />)
+        // document.querySelectorAll('.tasks-block').item(2).append(x)
+    }
     return (
         <>
             <div className="tasks-block" style={mainColor}>
                 <div className="tasks-block-line" style={borderColor}></div>
-                <div className="tasks-block__header">
-                    <img src={cardsIcons} />
-                    <h2 className="tasks-block__title">{cardsTitle}</h2>
+                <div >
+                    <div className="tasks-block__header">
+                        <img src={cardsIcons} />
+                        <h2 className="tasks-block__title">{cardsTitle}</h2>
+                    </div>
+                    <div className="btns">
+                        <button className='fr' onClick={test}>+</button>
+                        <button>-</button>
+                    </div>
+
                 </div>
-                
                 {Task}
+
             </div>
         </>
     );
