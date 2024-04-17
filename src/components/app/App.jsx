c2527b0fa1fb594b7162a1e3c64383486c43f5f9
Maxim93mk <kinjashov.m@rambler.ru>
2024-04-17 11:51:08 +0300

    Данные задач вынесены в отдельный файл data.js

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,47 +2,11 @@ import './App.css';
 import React, { useState } from 'react';
 import TasksMain from '../tasks/tasks';
 import { fragments } from './fragmentStyle.js';
-import user_1 from "./img/foto_1.svg";
-import user_2 from "./img/foto_2.svg";
+import { dataList } from './data.js';
 
 function App() {
-  let db = [
-    {
-      id: 1,
-      cards: [
-        'To Do',
-        'In Progress',
-        'Review',
-        'Done',
-      ],
-      tasks: [
-        'Christmas Banners',
-        'Redo Portfolio',
-        'Coffee Break',
-        'Updating Portfolio',
-        'Release to Figma Community',
-        'User Feedback',
-        'Background images from humaaans.com',
-        'Style Guide',
-        'Component Library',
-        'Sticker Components',
-      ],
-      status: [
-        'Label',
-        'Always',
-        'Release',
-        'Feedback',
-        'Sourcing',
-        'UI',
-      ],
-      user: [
-        user_1,
-        user_2,
-      ]
-    }
-  ];
-
-  const [data, setNotes] = useState(db);
+  
+ const [data, setNotes] = useState(dataList.data);
 
   let styleCards = [
     fragments.fragment_toDo_main_color,
---
+++ src/components/app/data.js
@@ -0,0 +1,42 @@
+import user_1 from "./img/foto_1.svg";
+import user_2 from "./img/foto_2.svg";
+
+let data = [
+    {
+      id: 1,
+      cards: [
+        'To Do',
+        'In Progress',
+        'Review',
+        'Done',
+      ],
+      tasks: [
+        'Christmas Banners',
+        'Redo Portfolio',
+        'Coffee Break',
+        'Updating Portfolio',
+        'Release to Figma Community',
+        'User Feedback',
+        'Background images from humaaans.com',
+        'Style Guide',
+        'Component Library',
+        'Sticker Components',
+      ],
+      status: [
+        'Label',
+        'Always',
+        'Release',
+        'Feedback',
+        'Sourcing',
+        'UI',
+      ],
+      user: [
+        user_1,
+        user_2,
+      ]
+    }
+  ];
+
+export  const dataList = {
+    data: data
+} 
